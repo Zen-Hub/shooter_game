548cd1aa5ef7f3f8a688a60694ee2b428495733a
Zen-Hub <inboxshow1@gmail.com>
2021-08-09 14:48:22 +0300

    added end game in js

--- main.js
+++ main.js
@@ -5,6 +5,21 @@ document.addEventListener('keydown', (e) => {
     jump();
 })
 
-function jump(){
-warrior.classList.add('jump')
+function jump() {
+    if (warrior !== 'jump') {
+        warrior.classList.add('jump')
+    };
+    setTimeout(function () {
+        warrior.classList.remove('jump')
+    }, 300);
 }
+
+let isAllive = setInterval(function(){
+    let warriorTop = parseInt(window.getComputedStyle(warrior).getPropertyValue("top"));
+    let zombieLeft = parseInt(window.getComputedStyle(zombie).getPropertyValue("left"));
+    console.log(warriorTop)
+
+    if(zombieLeft<50&&zombieLeft>0&&warriorTop>=140){
+        alert('GAME OVER')
+    }
+},10)
